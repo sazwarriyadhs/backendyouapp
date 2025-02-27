cc1db9f3eb54f0e22749124033605cceaaecaa83
sazwarriyadhs <sazwarriyadh@gmail.com>
2025-02-27 16:26:07 +0700

    Initial commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,33 @@
-import { NextResponse } from 'next/server';
+# dependencies
+/node_modules
+/.pnp
+.pnp.js
+
+# testing
+/coverage
+
+# next.js
+/.next/
+/out/
+
+# production
+/build
+
+# misc
+.DS_Store
+*.pem
+
+# debug
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+
+# local env files
+.env*.local
+
+# typescript
+*.tsbuildinfo
+next-env.d.tsimport { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 const publicPaths = ['/login', '/register', '/forgot-password'];
